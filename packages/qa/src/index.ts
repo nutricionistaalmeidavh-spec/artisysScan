20aab8bb493e844f9f7f4b8845ba23c90064248c
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-18 19:33:01 -0300

    feat: expose QA engine

---
+++ packages/qa/src/index.ts
@@ -0,0 +1,17 @@
+export { discoverQaProject } from './discovery.js';
+export { runQa, type RunQaOptions } from './engine.js';
+export { createPlaywrightCaptureConfig, createQaPlan } from './plan.js';
+export { classifyQaArtifacts, summarizePlaywrightJson } from './results.js';
+export { runQaCommand } from './runner.js';
+export type {
+  PlaywrightSummary,
+  QaArtifacts,
+  QaCaptures,
+  QaCommand,
+  QaCommandResult,
+  QaDiscovery,
+  QaMode,
+  QaPlan,
+  QaReport,
+  QaRunner,
+} from './types.js';
