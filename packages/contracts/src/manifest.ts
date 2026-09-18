e913aa2e83e0edb62aaf9916cdcb3e8cc3b0e536
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-18 19:15:44 -0300

    fix: use named AJV constructor export

--- packages/contracts/src/manifest.ts
+++ packages/contracts/src/manifest.ts
@@ -1,6 +1,6 @@
 import { readFile } from 'node:fs/promises';
 
-import Ajv, { type ErrorObject } from 'ajv/dist/ajv.js';
+import { Ajv, type ErrorObject } from 'ajv';
 import { parse } from 'yaml';
 
 import schema from '../schema/artisys-scan.schema.json' with { type: 'json' };
