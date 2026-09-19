a21d1974e1270d59e5082a610e050fe050d3f52a
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-19 12:48:24 -0300

    chore(qa): export Windows spawn resolver

--- packages/qa/src/index.ts
+++ packages/qa/src/index.ts
@@ -2,7 +2,7 @@ export { discoverQaProject } from './discovery.js';
 export { runQa, type RunQaOptions } from './engine.js';
 export { createPlaywrightCaptureConfig, createQaPlan } from './plan.js';
 export { classifyQaArtifacts, summarizePlaywrightJson } from './results.js';
-export { runQaCommand } from './runner.js';
+export { resolveQaSpawnCommand, runQaCommand } from './runner.js';
 export type {
   PlaywrightSummary,
   QaArtifacts,
